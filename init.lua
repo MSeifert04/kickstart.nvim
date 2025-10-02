2be790caffb706ad9e9129f544a3aaa1b63f4cd4
Michael Seifert-Eckert <seifert@poolarserver.com>
2025-10-02 23:17:55 +0200

    4 spaces instead of tabs

--- init.lua
+++ init.lua
@@ -166,6 +166,12 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- No tabs, just spaces, 4 by default
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
